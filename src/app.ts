cc5b580cf5d212ed16db0f12bafa6c3b0ea7d6a6
David J. Zielinski <djzielin@gmail.com>
2020-04-03 08:02:29 +0000

    multiusers still not workign

--- src/app.ts
+++ src/app.ts
@@ -24,10 +24,13 @@ export default class HelloWorld {
 	private ourRightHand: MRE.Actor = null;
 
 	private ourSounds: MRE.Sound[] = [];
+	private ourHands: MRE.Actor[] = [];
+	private hand1: MRE.Actor;
+	private hand2: MRE.Actor;
 
 	private userID: string;
 	private counter = 0;
-
+	private handCounter=0;
 	constructor(private context: MRE.Context, private baseUrl: string) {
 		MRE.log.info("app", "our constructor started");
 		this.assets = new MRE.AssetContainer(context);
@@ -38,48 +41,80 @@ export default class HelloWorld {
 	}
 
 	private userJoined(user: MRE.User) {
+		/*if(this.handCounter===0){
+			MRE.log.info("app","skipping adding hand for: " + user.name);
+			this.handCounter++;
+			return;
+		}*/
 		MRE.log.info("app", "user joined. name: " + user.name + " id: " + user.id);
 
 		const rHand = MRE.Actor.Create(this.context, {
 			actor: {
+				name: "rHand actor for user " + user.name,
 				transform: {
+					//app: { position: new MRE.Vector3(0,0.0,0.0)},
 					local: { position: new MRE.Vector3(0,0.0,0.2) }
 				},
 				attachment: {
 					attachPoint: 'right-hand',
 					userId: user.id
-				},
-				//subscriptions: ['transform']
+				}
 			}
 		});
-		if(rHand) {
+		if (rHand) {
 			MRE.log.info("app", "   added their right hand");
-			rHand.subscribe('transform');
-			this.allRightHands.set(user.id, rHand);
+
+			//if (this.handCounter === 1) {
+				rHand.subscribe('transform');
+
+				for (let i = 0; i < rHand.subscriptions.length; i++) {
+					MRE.log.info("app", "     subscription: " + rHand.subscriptions[i]);
+				}
+				this.allRightHands.set(user.id, rHand);
+				this.ourHands.push(rHand);
+				if(this.handCounter===0){
+					this.hand1=rHand;
+				}else{
+					this.hand2=rHand;
+				}
+
+
+			//}
 		} else {
 			MRE.log.info("app", "   ERROR during hand creation!!");
 		}
-
+/*
 		const lHand = MRE.Actor.Create(this.context, {
 			actor: {
+				name: "lHand actor for user " + user.name,
 				transform: {
+					//app: { position: new MRE.Vector3(0,0.0,0.0)},
 					local: { position: new MRE.Vector3(0,0.0,0.2) }
 				},
 				attachment: {
 					attachPoint: 'left-hand',
 					userId: user.id
-				},
-				//subscriptions: ['transform']
+				}
 			}
 		});
 		
-		if(lHand) {
+		if (lHand) {
 			MRE.log.info("app", "   added their left hand");
-			lHand.subscribe('transform');
-			this.allLeftHands.set(user.id, lHand);
+
+			//if (this.handCounter === 1) {
+				lHand.subscribe('transform');
+
+				for (let i = 0; i < lHand.subscriptions.length; i++) {
+					MRE.log.info("app", "     subscription: " + lHand.subscriptions[i]);
+				}
+
+				this.allLeftHands.set(user.id, lHand);
+			//}
 		} else {
 			MRE.log.info("app", "   ERROR during hand creation!!");
 		}
+*/
+		this.handCounter++;
 	}
 
 	private userLeft(user: MRE.User) {
@@ -118,6 +153,11 @@ export default class HelloWorld {
 				closestDist = hDist;
 				closestActor = hand;
 				closestIndex = index;
+			}
+
+			for(let i=0;i<hand.subscriptions.length;i++)
+			{
+				MRE.log.info("app","    subscription: " + hand.subscriptions[i]);
 			}
 			index++;
 		}
@@ -159,6 +199,7 @@ export default class HelloWorld {
 			}
 		});
 
+		/*
 		setInterval(() => {
 			if (this.ourRightHand) {
 				this.rightSoundHand.updateSound(this.ourRightHand.transform.app.position);
@@ -167,11 +208,22 @@ export default class HelloWorld {
 				this.leftSoundHand.updateSound(this.ourLeftHand.transform.app.position);
 			}
 		}, 30); //fire every 30ms
+		*/
 
 		//keep checking who has the closest hand to theremin. put that hand in charge
 		setInterval(() => {
-			this.ourRightHand = this.findClosestHand("righthand",this.allRightHands);
-			this.ourLeftHand = this.findClosestHand("lefthand",this.allLeftHands);
+			/*let i=0;
+			for(let hand of this.ourHands)
+			{   hand.subscribe('transform');
+				MRE.log.info("app","hand " + i + " + pos: " + hand.transform.app.position);
+				i++;
+			}*/
+			MRE.log.info("app","hand0: " + this.hand1.transform.app.position);
+			MRE.log.info("app","hand1: " + this.hand2.transform.app.position);
+			this.hand1.subscribe('transform');
+			this.hand2.subscribe('transform');
+			//this.ourRightHand = this.findClosestHand("righthand",this.allRightHands);
+			//this.ourLeftHand = this.findClosestHand("lefthand",this.allLeftHands);
 		}, 1000); //fire every 1 sec
 	}
 }
